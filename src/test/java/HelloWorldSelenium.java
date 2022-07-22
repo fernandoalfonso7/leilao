180e00b0587bc8bada3f6a2b2e08e5ba8b4fb4eb
fernandoalfonso7 <alfonso.fernando@hotmail.com>
2022-07-22 00:24:30 -0300

    projeto

---
+++ src/test/java/HelloWorldSelenium.java
@@ -0,0 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldSelenium {
+
+    @Test
+    public void hello(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("http://localhost:8080/leiloes");
+        //browser.quit();
+    }
+
+}
